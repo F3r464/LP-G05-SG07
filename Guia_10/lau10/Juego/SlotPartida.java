309d50a46c3c7b5dce3e00b786a5a387d7d4697a
F3r464 <f3rn4nd020w14infinite@gmail.com>
2025-11-27 09:47:33 -0500

    Update SlotPartida.java

--- Guia_10/lau10/Juego/SlotPartida.java
+++ Guia_10/lau10/Juego/SlotPartida.java
@@ -1,25 +1,33 @@
 import javax.swing.*;
+
 public class SlotPartida extends JButton {
+
     public SlotPartida(Partida p) {
+
         setLayout(null);
         setBorderPainted(false);
         setContentAreaFilled(false);
+
         setIcon(new ImageIcon("assets/slot_base.png"));
         setRolloverIcon(new ImageIcon("assets/slot_base_hover.png"));
         setSize(600, 140);
 
-        if(p != null){
-            JLabel nombre = new JLabel(new ImageIcon(TextoImagen.crearTexto("Jugador: " + p.nombre, 26)));
+        if (p != null) {
+
+            JLabel nombre = new JLabel(new ImageIcon("assets/slot_nombre.png"));
             nombre.setBounds(40, 20, 400, 40);
             add(nombre);
-            JLabel tiempo = new JLabel(new ImageIcon(TextoImagen.crearTexto("Tiempo: " + p.tiempo, 20)));
+
+            JLabel tiempo = new JLabel(new ImageIcon("assets/slot_tiempo.png"));
             tiempo.setBounds(40, 65, 300, 40);
             add(tiempo);
-            JLabel escenario = new JLabel(new ImageIcon(TextoImagen.crearTexto("Escenario: " + p.escenario, 20)));
+
+            JLabel escenario = new JLabel(new ImageIcon("assets/slot_escenario.png"));
             escenario.setBounds(260, 65, 300, 40);
             add(escenario);
+
         } else {
-            JLabel vacio = new JLabel(new ImageIcon(TextoImagen.crearTexto("Vacío", 28)));
+            JLabel vacio = new JLabel(new ImageIcon("assets/slot_vacio.png"));
             vacio.setBounds(230, 45, 200, 40);
             add(vacio);
         }
